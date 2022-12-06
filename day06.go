6caf0fadc1c276074cf01fc6375f9e1f168c7fae
Osarodion Irabor <33585132+lightning-dabbler@users.noreply.github.com>
2022-12-06 00:55:41 -0500

    day 6 brute force

---
+++ day06.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+)
+
+//go:embed day06-input.txt
+var s string
+
+func partA() {
+	characterIndex := 1
+	n := len(s)
+	for i := 0; i < n-4; i++ {
+		v := s[i]
+		unique := true
+		characterIndex = i + 1
+		Map := map[byte]struct{}{v: {}}
+		for j := i + 1; j < i+4; j++ {
+			_, ok := Map[s[j]]
+			if ok {
+				unique = false
+				break
+			} else {
+				Map[s[j]] = struct{}{}
+				characterIndex += 1
+			}
+		}
+		if unique {
+			break
+		}
+	}
+	fmt.Println(characterIndex)
+}
+
+func partB() {
+	characterIndex := 1
+	n := len(s)
+	for i := 0; i < n-14; i++ {
+		v := s[i]
+		unique := true
+		characterIndex = i + 1
+		Map := map[byte]struct{}{v: {}}
+		for j := i + 1; j < i+14; j++ {
+			_, ok := Map[s[j]]
+			if ok {
+				unique = false
+				break
+			} else {
+				Map[s[j]] = struct{}{}
+				characterIndex += 1
+			}
+		}
+		if unique {
+			break
+		}
+	}
+	fmt.Println(characterIndex)
+}
+
+func main() {
+	partA()
+	partB()
+}
